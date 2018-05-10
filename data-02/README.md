aef923000dde7843f96ff96b6b6047e3a2133182
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2018-05-10 09:36:35 +0200

    Add new case, now building components for showing data.

---
+++ data-02/README.md
@@ -0,0 +1,30 @@
+
+*[Back to the main page](../README.md)*
+
+## Visualizing data
+
+Now, I'm going to work in `data-02`,
+visualizing some data points with a scatterplot
+(each dot will be an sphere), with an axis.
+But now, I will be using A-frame components
+for the spheres and the axis.
+
+We will follow the documentation in
+[Writing a component](https://aframe.io/docs/0.8.0/introduction/writing-a-component.html)
+
+### Generating dist files and running everything
+
+As we saw in the previous section, the complete process to build the project is:
+
+```
+$ cd data-02
+$ npm install
+$ npm run start
+```
+
+This will build everything,
+you will only need a browser to see the resulting scene.
+
+### Results
+
+Check the resulting [virtual reality scene](web/index.html).
---
+++ data-02/webpack.config.js
@@ -0,0 +1,42 @@
+// Required libraries, for webpack to work
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/data.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // JavaScript files
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      },
+      // HTML files
+      {
+        test: /\.html$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: ['html-loader']
+      }
+    ],
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/data.html',
+      inject: 'head',
+      filename: 'index.html'
+    })
+  ]
+};

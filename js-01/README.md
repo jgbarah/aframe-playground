048299fecfb05bfd4b78ee7f17631f55018ca626
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2021-12-15 23:46:24 +0100

    [js-01] Adding first example

---
+++ js-01/README.md
@@ -0,0 +1,14 @@
+
+*[Back to the main page](../README.md)*
+
+## Building a scene with JavaScript
+
+Let's work in `js-01`, creating a scene from scratch using
+JavaScript. In other words, in HTML will have only an
+`a-scene` element, and a JS script that, when the HTML
+document is loaded in the browser, injects the rest of the
+scene in the DOM, as children of `a-secene`.
+
+### Results
+
+Check the resulting [virtual reality scene](scene1.html).
---
+++ js-01/scene1.js
@@ -0,0 +1,28 @@
+// Inject some A-Frame entities in a scene
+
+let scene = document.querySelector('a-scene');
+scene.addEventListener('loaded', function () {
+    // Box
+    // <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+    let box = document.createElement('a-box');
+    box.setAttribute('color', 'red');
+    box.setAttribute('position', {x: -1, y: 0.5, z: -3});
+    box.setAttribute('rotation', {x: 0, y: 45, z: 0});
+    box.setAttribute('color', "#4CC3D9");
+    scene.appendChild(box); 
+
+    // Sphere
+    // <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+    let sphere = document.createElement('a-sphere');
+    sphere.setAttribute('position', {x:0, y: 1.25, z: -5});
+    sphere.setAttribute('radius', 1.25);
+    sphere.setAttribute('color', "#EF2D5E");
+    scene.appendChild(sphere);
+
+    // Cylinder
+    // <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+
+    // Plane
+    // <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+});
+
